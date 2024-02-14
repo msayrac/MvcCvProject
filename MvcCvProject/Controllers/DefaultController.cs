eeac816b0d480e9b21d05b29a1e8cb0a4cbfa644
msayrac <msayracode@gmail.com>
2024-02-14 10:08:44 +0300

    left side bar added. iletisim and sendmessage added

--- MvcCvProject/Controllers/DefaultController.cs
+++ MvcCvProject/Controllers/DefaultController.cs
@@ -10,17 +10,79 @@ using MvcCvProject.Models.Entity;
 
 namespace MvcCvProject.Controllers
 {
-    public class DefaultController : Controller
-    {
-        // GET: Default
+	public class DefaultController : Controller
+	{
+		// GET: Default
 
-        DbCvEntities db = new DbCvEntities();
-        public ActionResult Index()
-        {
+		DbCvEntities db = new DbCvEntities();
+		public ActionResult Index()
+		{
 
-           var degerler = db.TblHakkimda.ToList();
+			var degerler = db.TblHakkimda.ToList();
 
-            return View(degerler);
-        }
-    }
+			return View(degerler);
+		}
+
+		public PartialViewResult Deneyim()
+		{
+
+			var deneyimler = db.TblDeneyimlerim.ToList();
+
+			return PartialView(deneyimler);
+		}
+
+		public PartialViewResult Egitimler()
+		{
+			var egitimler = db.TblEgitimlerim.ToList();
+			return PartialView(egitimler);
+		}
+
+		public PartialViewResult Yeteneklerim()
+		{
+			var yetenekler = db.TblYeteneklerim.ToList();
+
+			return PartialView(yetenekler);
+		}
+
+		public PartialViewResult Hobilerim()
+		{
+			var hobilerim = db.TblHobilerim.ToList();
+
+			return PartialView(hobilerim);
+		}
+
+		public PartialViewResult Sertifikalarim()
+		{
+			var sertifika = db.Sertifikalarim.ToList();
+
+			return PartialView(sertifika);
+		}
+
+		[HttpGet]
+		public PartialViewResult Iletisim()
+		{
+			var iletisim = db.TblIletisim.ToList();
+
+			return PartialView(iletisim);
+		}
+
+		[HttpPost]
+		public PartialViewResult Iletisim(TblIletisim t)
+		{
+
+			t.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+
+			db.TblIletisim.Add(t);
+			
+			db.SaveChanges();
+
+			return PartialView();
+		}
+
+
+
+
+
+
+	}
 }
