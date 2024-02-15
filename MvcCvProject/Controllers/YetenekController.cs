1737d4e60187a23926870f511c8f71c580fe7873
msayrac <msayracode@gmail.com>
2024-02-15 23:19:27 +0300

    Yeteneklerim completed on admin panel

--- MvcCvProject/Controllers/YetenekController.cs
+++ MvcCvProject/Controllers/YetenekController.cs
@@ -8,26 +8,63 @@ using System.Web.Mvc;
 
 namespace MvcCvProject.Controllers
 {
-    public class YetenekController : Controller
-    {
-        // GET: Yetenek
-        YetenekRepository repo = new YetenekRepository();
+	public class YetenekController : Controller
+	{
+		// GET: Yetenek
+		YetenekRepository repo = new YetenekRepository();
 
-        public ActionResult Index()
-        {
-            var yetenekler = repo.List();
+		public ActionResult Index()
+		{
+			var yetenekler = repo.List();
 
-            return View(yetenekler);
-        }
+			return View(yetenekler);
+		}
 
-        public ActionResult YetenekSil(int id)
-        {
-            TblYeteneklerim t =repo.Find(x=>x.ID == id);
-            
-            repo.TDelete(t);
+		[HttpGet]
+		public ActionResult YeniYetenek()
+		{
+			return View();
+		}
 
-            return RedirectToAction("Index");
+		[HttpPost]
+		public ActionResult YeniYetenek(TblYeteneklerim p)
+		{
+			repo.TAdd(p);
+			return RedirectToAction("Index");
+		}
 
-        }
-    }
+		public ActionResult YetenekSil(int id)
+		{
+			TblYeteneklerim t = repo.Find(x => x.ID == id);
+
+			repo.TDelete(t);
+
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet]
+		public ActionResult YetenekDuzenle(int id)
+		{
+			var t = repo.Find(x => x.ID == id);
+
+			return View(t);
+		}
+
+		[HttpPost]
+		public ActionResult YetenekDuzenle(TblYeteneklerim t)
+		{
+			var y =repo.Find(x=>x.ID== t.ID);
+			y.Yetenek = t.Yetenek;
+			y.Oran = t.Oran;
+
+			repo.TUpdate(y);
+
+			
+			return RedirectToAction("Index");
+		}
+
+
+
+
+	}
 }
