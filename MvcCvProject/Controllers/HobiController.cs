27cadfb043b0008163ce37133065089fc6521d7c
msayrac <msayracode@gmail.com>
2024-02-18 21:22:05 +0300

    Hakkimda page added

---
+++ MvcCvProject/Controllers/HobiController.cs
@@ -0,0 +1,40 @@
+﻿using MvcCvProject.Models.Entity;
+using MvcCvProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCvProject.Controllers
+{
+	public class HobiController : Controller
+	{
+		// GET: Hobi
+
+		HobiRepository repo = new HobiRepository();
+
+		[HttpGet]
+		public ActionResult Index()
+		{
+			var hobiler = repo.List();
+			return View(hobiler);
+		}
+
+		[HttpPost]
+		public ActionResult Index(TblHobilerim p)
+		{
+
+			var t = repo.Find(x=>x.ID== 1);
+
+			t.Aciklama1 = p.Aciklama1;
+			t.Aciklama2 = p.Aciklama2;
+			repo.TUpdate(t);
+
+			return RedirectToAction("Index");
+		}
+
+
+
+	}
+}
---
+++ MvcCvProject/Controllers/iletisimController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Ajax.Utilities;
+using MvcCvProject.Models.Entity;
+using MvcCvProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCvProject.Controllers
+{
+	public class iletisimController : Controller
+	{
+		// GET: iletisim
+
+		GenericRepository<TblIletisim> iletisimRepo = new GenericRepository<TblIletisim>();
+
+		public ActionResult Index()
+		{
+			var iletisim = iletisimRepo.List();
+			return View(iletisim);
+		}
+
+
+
+	}
+}
