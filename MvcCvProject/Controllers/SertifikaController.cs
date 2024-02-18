78d7ba9efa809e75931b6c5dabd898f35c7c6f3b
msayrac <msayracode@gmail.com>
2024-02-18 13:37:35 +0300

    Sertifika admin page added

---
+++ MvcCvProject/Controllers/SertifikaController.cs
@@ -0,0 +1,67 @@
+﻿using MvcCvProject.Models.Entity;
+using MvcCvProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCvProject.Controllers
+{
+	public class SertifikaController : Controller
+	{
+		// GET: Sertifika
+
+		SertifikaRepository repo = new SertifikaRepository();
+		public ActionResult Index()
+		{
+			var sertifikalar = repo.List();
+
+			return View(sertifikalar);
+		}
+
+		[HttpGet]
+		public ActionResult SertifikaEkle()
+		{
+
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult SertifikaEkle(Sertifikalarim s)
+		{
+			repo.TAdd(s);
+
+			return RedirectToAction("Index");
+		}
+
+		public ActionResult SertifikaSil(int id)
+		{
+			Sertifikalarim s = repo.Find(x => x.ID == id);
+			repo.TDelete(s);
+			return RedirectToAction("Index");
+		}
+
+
+		[HttpGet]
+		public ActionResult SertifikaDuzenle(int id)
+		{
+			
+			Sertifikalarim s = repo.Find(x => x.ID == id);
+			return View(s);
+		}
+
+		[HttpPost]
+		public ActionResult SertifikaDuzenle(Sertifikalarim sertifika)
+		{
+			Sertifikalarim s = repo.Find(x => x.ID == sertifika.ID);
+			s.Tarih = sertifika.Tarih;
+			s.Aciklama = sertifika.Aciklama;
+
+			repo.TUpdate(s);
+
+			return RedirectToAction("Index");
+		}
+
+	}
+}
--- MvcCvProject/Models/Entity/TblEgitimlerim.cs
+++ MvcCvProject/Models/Entity/TblEgitimlerim.cs
@@ -11,26 +11,14 @@ namespace MvcCvProject.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-	using System.ComponentModel.DataAnnotations;
-
+    
     public partial class TblEgitimlerim
     {
         public int Id { get; set; }
-        [Required(ErrorMessage ="Bu alaný boþ geçemezsiniz")]
         public string Baslik { get; set; }
-
-		[Required(ErrorMessage = "Bu alaný boþ geçemezsiniz")]
-	
-		public string AltBaslik1 { get; set; }
-
-		[Required(ErrorMessage = "Bu alaný boþ geçemezsiniz")]
-		public string AltBaslik2 { get; set; }
-
-		[Required(ErrorMessage = "Bu alaný boþ geçemezsiniz")]
-		[StringLength(10,ErrorMessage="Lütfen en fazla 10 karakterlik veri giriþi yapýn")]
-		public string GNO { get; set; }
-
-		[Required(ErrorMessage = "Bu alaný boþ geçemezsiniz")]
-		public string Tarih { get; set; }
+        public string AltBaslik1 { get; set; }
+        public string AltBaslik2 { get; set; }
+        public string GNO { get; set; }
+        public string Tarih { get; set; }
     }
 }
